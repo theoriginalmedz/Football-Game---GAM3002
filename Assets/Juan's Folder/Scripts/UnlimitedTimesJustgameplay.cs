a5789bfc5d386a63a2186225277eb70201e64eb7
Medz <madmedz@hotmail.co.uk>
2020-05-13 13:46:07 +0100

    Update 1.07 - Updated Game, new mode. Final
    This is the final update for this project. Updated the files and added a new endless shootout scenario mode.

---
+++ Assets/Juan's Folder/Scripts/UnlimitedTimesJustgameplay.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UnlimitedTimesJustgameplay : MonoBehaviour
+{
+    [SerializeField]
+    GameObject musicMana;
+
+    public void Awake()
+    {
+       musicMana.SetActive(true);
+        
+    }
+
+    public void playGameAgain()
+    {
+        SceneManager.LoadScene(32); //make this scene the scene that the unlimited game is on
+    }
+
+    public void backToMenu()
+    {
+        
+        SceneManager.LoadScene(0); //make this scene the next scene you go to when you want to go home
+        musicMana.SetActive(false);
+
+
+    }
+}
---
+++ Assets/Juan's Folder/Scripts/musicManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class musicManager : MonoBehaviour
+{ 
+
+
+    private GameObject[] musicGame;
+
+    void Start()
+    {
+        musicGame = GameObject.FindGameObjectsWithTag("GameMusic");
+        Destroy(musicGame[1]);
+    }
+
+    // Update is called once per frame
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+}
